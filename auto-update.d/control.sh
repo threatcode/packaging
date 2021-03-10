424db519df5fab39d63e840c7e94832cf3203cb3
Arnaud Rebillout <arnaudr@kali.org>
2021-03-10 13:06:57 +0700

    Use variables for control/copyright files
    For style consistency with other files in this directory.

--- auto-update.d/control.sh
+++ auto-update.d/control.sh
@@ -1,6 +1,9 @@
 # This file is sourced by bin/auto-update
 
-SOURCE=$(awk '/^Source:/ {print $2}' debian/control)
+control_file=debian/control
+copyright_file=debian/copyright
+
+SOURCE=$(awk '/^Source:/ {print $2}' $control_file)
 CURDIR_NAME=$(basename $PWD)
 
 if [ "$CURDIR_NAME" != "$SOURCE" ]; then
@@ -8,28 +11,28 @@ if [ "$CURDIR_NAME" != "$SOURCE" ]; then
 fi
 
 # Maintainer
-if grep -q -E "Maintainer: .*(kali|offensive-security|hertzog|sophie@freexian)" debian/control; then
-    sed -i -e "s|^Maintainer: .*|Maintainer: Kali Developers <devel@kali.org>|i" debian/control
+if grep -q -E "Maintainer: .*(kali|offensive-security|hertzog|sophie@freexian)" $control_file; then
+    sed -i -e "s|^Maintainer: .*|Maintainer: Kali Developers <devel@kali.org>|i" $control_file
 else
-    sed -i -e "s|^Maintainer: \(.*\)|XSBC-Original-Maintainer: \1\nMaintainer: Kali Developers <devel@kali.org>|i" debian/control
+    sed -i -e "s|^Maintainer: \(.*\)|XSBC-Original-Maintainer: \1\nMaintainer: Kali Developers <devel@kali.org>|i" $control_file
 fi
-record_change "Update Maintainer field" debian/control
+record_change "Update Maintainer field" $control_file
 
 # Vcs-*
 sed -i \
     -e "s|^Vcs-Git: .*|Vcs-Git: https://gitlab.com/kalilinux/packages/$SOURCE.git|i" \
     -e "s|^Vcs-Browser: .*|Vcs-Browser: https://gitlab.com/kalilinux/packages/$SOURCE|i" \
-    debian/control
-record_change "Update Vcs-* fields" debian/control
+    $control_file
+record_change "Update Vcs-* fields" $control_file
 
 # Homepage
 sed -i \
     -e "s|^Homepage: http://www.kali.org|Homepage: https://www.kali.org|i" \
-    debian/control
-if [ -e debian/copyright ]; then
+    $control_file
+if [ -e $copyright_file ]; then
     sed -i \
 	-e "s|^Source: http://www.kali.org|Source: https://www.kali.org|i" \
-	debian/copyright
-    extra=debian/copyright
+	$copyright_file
+    extra=$copyright_file
 fi
-record_change "Use HTTPS url for www.kali.org" debian/control $extra
+record_change "Use HTTPS url for www.kali.org" $control_file $extra
