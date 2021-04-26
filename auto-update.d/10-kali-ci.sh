a376ee8c43c477ffd9feab2388eee02b5048bc41
Raphaël Hertzog <raphael@offensive-security.com>
2021-04-26 15:26:10 +0200

    Make it possible to entirely disable the Kali CI by renaming the file

--- auto-update.d/10-kali-ci.sh
+++ auto-update.d/10-kali-ci.sh
@@ -2,14 +2,18 @@
 
 ci_file=debian/kali-ci.yml
 
-create_if_missing $ci_file <<END
+if [ ! -e $ci_file.disabled ]; then
+
+    create_if_missing $ci_file <<END
 include:
   - https://gitlab.com/kalilinux/tools/kali-ci-pipeline/raw/master/recipes/kali.yml
 END
-record_change "Add GitLab's CI configuration file" $ci_file
+    record_change "Add GitLab's CI configuration file" $ci_file
 
-if grep -q 'gitlab-ci/kali/templates.yml' $ci_file; then
-    sed -i -e 's|gitlab-ci/kali/templates.yml|recipes/kali.yml|' \
-	   -e '/pipeline-jobs.yml/d' $ci_file
-    record_change "Update URL in GitLab's CI configuration file" $ci_file
+    if grep -q 'gitlab-ci/kali/templates.yml' $ci_file; then
+	sed -i -e 's|gitlab-ci/kali/templates.yml|recipes/kali.yml|' \
+	       -e '/pipeline-jobs.yml/d' $ci_file
+	record_change "Update URL in GitLab's CI configuration file" $ci_file
+    fi
+
 fi
